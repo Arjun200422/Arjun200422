978805bcacf02ef816d84f9afe079c41780422c5
aswin-1016 <aswint1905@gmail.com>
2025-03-18 12:02:27 +0530

    Initial commit

--- src/main/java/com/ecom/Customer/Order/CustomerOrderApplication.java
+++ src/main/java/com/ecom/Customer/Order/CustomerOrderApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CustomerOrderApplication {
+public class CustomerOrderApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerOrderApplication.class, args);
